fc4ece51af879c95e5f03e407f11e0ce46bc7da0
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-09-06 09:37:02 -0300

    Update for axios

--- backend/BuyTheProductController.js
+++ backend/BuyTheProductController.js
@@ -1,5 +1,5 @@
 const DatabasePayers = require('./DatabasePayers');
-const { MercadoPagoConfig, Preference } = require('mercadopago');
+const axios = require('axios'); // Use axios ou outra biblioteca para HTTP requests
 const Cupom = require('./Cupom');
 
 const BuyTheProductController = async (req, res) => {
@@ -19,10 +19,8 @@ const BuyTheProductController = async (req, res) => {
     try {
         const porcent = cupomFound ? cupomFound.porcent : 0;
         const value = 21 - (21 * porcent / 100);
-        const client = new MercadoPagoConfig({ accessToken: 'APP_USR-8236944029478650-083009-a7d3d34f12c17b040f58853cb2f6dc9c-2655607003' });
 
-        const preference = new Preference(client);
-
+        // Configuração do corpo da requisição para a API do Mercado Pago
         const body = {
             items: [
                 {
@@ -33,16 +31,33 @@ const BuyTheProductController = async (req, res) => {
                     unit_price: value,
                 },
             ],
+            payment_methods: {
+                excluded_payment_types: [
+                    { id: 'ticket' } // Remove boleto
+                ],
+                // Opcional: Definir PIX como método preferido
+                default_payment_method_id: 'pix',
+            },
             notification_url: `https://pb-0t3x.onrender.com/webhook/${userName}/${personalized}/${email}/${code}/${blood}/${arlegies}`,
         };
 
-        await preference.create({ body }).then((response) => {
-            return res.send({ init_point: response.init_point });
-        });
+        // Configuração dos headers
+        const config = {
+            headers: {
+                'Authorization': `Bearer APP_USR-8236944029478650-083009-a7d3d34f12c17b040f58853cb2f6dc9c-2655607003`, // Seu access token
+                'Content-Type': 'application/json',
+            },
+        };
+
+        // Fazendo a requisição POST diretamente para a API
+        const response = await axios.post('https://api.mercadopago.com/checkout/preferences', body, config);
+        
+        // Retorna a URL de inicialização do checkout
+        res.send({ init_point: response.data.init_point });
 
     } catch (error) {
-        res.send('error');
-        console.error(error);
+        console.error('Erro ao criar preferência:', error.response ? error.response.data : error.message);
+        res.status(500).send('Erro interno do servidor');
     }
 }
 
