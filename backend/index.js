b60d9174add35752996ebedaa08b67655cacc089
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-09-08 23:38:14 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const connectToDb = require('./ConnectToDb');
+const axios = require('axios');
 
 const app = express();
 const PORT = 3000;
