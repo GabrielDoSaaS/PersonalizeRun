c72c97f158ee9dd486d33c237297e2dbf11d6999
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-09-09 00:01:49 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,13 @@ const connectToDb = require('./ConnectToDb');
 const mercadopago = require('mercadopago');
 const app = express();
 const PORT = 3000;
+const { MercadoPagoConfig, Preference, Payment } = require('mercadopago');
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-mercadopago.configurations.setAccessToken('APP_USR-7932112160870899-090608-086afe9324ef4d53debb58635846b322-1840600103');
+const mp = new MercadoPagoConfig('APP_USR-7932112160870899-090608-086afe9324ef4d53debb58635846b322-1840600103');
 
 app.post("/webhook/mercadopago", async (req, res) => {
   try {
@@ -20,7 +21,7 @@ app.post("/webhook/mercadopago", async (req, res) => {
       const paymentId = data.id;
       
       // Buscar detalhes do pagamento
-      const paymentInfo = await mercadopago.payment.findById(paymentId);
+      const paymentInfo = await mp.payment.findById(paymentId);
       const payment = paymentInfo.body;
 
       if (payment.status === "approved") {
