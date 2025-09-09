bc48b197137cca5828b3249fde0eb49f3cb20fc2
Gabriel-developments <224129097+GabrielDoSaaS@users.noreply.github.com>
2025-09-09 09:07:54 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -47,41 +47,7 @@ app.post("/webhook/mercadopago", async (req, res) => {
     res.sendStatus(500);
   }
 });
-    const payment = req.query;
 
-    console.log({payment});
-    const paymentId = payment.id;
-
-    try {
-        const response = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
-            method: "GET",
-            headers: {
-                "Authorization": "Bearer APP_USR-7932112160870899-090608-086afe9324ef4d53debb58635846b322-1840600103"
-            }
-        })
-
-        if(response.ok) {
-
-            const data = await response.json();
-
-            if(data.status == "approved") {
-                const metadata = response.metadata;
-                console.log({metadata});
-                const userName = metadata.userName;
-                const personalized = metadata.personalized;
-                const email = metadata.email;
-                const code = metadata.code;
-                const blood = metadata.blood;
-                const allergies = metadata.allergies;
-            }
-                
-             return res.sendStatus(200);
-                
-        }
-    } catch {
-        res.sendStatus(500);
-    }
-});
 
 connectToDb();
 
